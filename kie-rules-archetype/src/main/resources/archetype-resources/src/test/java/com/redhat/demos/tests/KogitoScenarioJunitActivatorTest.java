32999f8ce652aba63c49ade689c1bdf7893a8e4f
Rafael T. C. Soares <rafaelcba@gmail.com>
2021-02-12 10:24:24 -0500

    adding kogito junit test

---
+++ kie-rules-archetype/src/main/resources/archetype-resources/src/test/java/com/redhat/demos/tests/KogitoScenarioJunitActivatorTest.java
@@ -0,0 +1,7 @@
+package com.redhat.demos.tests;
+/**
+* Use this class if you play to run scesim on VSCode with Kogito.
+*/
+@org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
+public class ScenarioJunitActivatorTest {
+}
--- kie-rules-archetype/src/main/resources/archetype-resources/src/test/java/com/redhat/demos/tests/ScenarioJunitActivatorTest.java
+++ kie-rules-archetype/src/main/resources/archetype-resources/src/test/java/com/redhat/demos/tests/ScenarioJunitActivatorTest.java
@@ -1,6 +1,7 @@
 package com.redhat.demos.tests;
 /**
-* Do not remove this file
+* Use this class if you play to run scesim on Business Central with Drools.
+* Remember to comment out the Drools scenariosimulation deps in pom.xml!
 */
 @org.junit.runner.RunWith(org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator.class)
 public class ScenarioJunitActivatorTest {
