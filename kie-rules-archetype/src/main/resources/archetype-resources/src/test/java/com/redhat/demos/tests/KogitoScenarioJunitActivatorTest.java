5592a5c1e9834e50537ad28b791fef7e2998fad2
Rafael T. C. Soares <rafaelcba@gmail.com>
2021-03-09 16:19:49 -0500

    commenting out Kogito test runner

--- kie-rules-archetype/src/main/resources/archetype-resources/src/test/java/com/redhat/demos/tests/KogitoScenarioJunitActivatorTest.java
+++ kie-rules-archetype/src/main/resources/archetype-resources/src/test/java/com/redhat/demos/tests/KogitoScenarioJunitActivatorTest.java
@@ -2,6 +2,7 @@ package com.redhat.demos.tests;
 /**
 * Use this class if you play to run scesim on VSCode with Kogito.
 */
-@org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
-public class ScenarioJunitActivatorTest {
+// uncomment when executing from maven or vscode
+// @org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
+public class KogitoScenarioJunitActivatorTest {
 }
