340ab8a1a74175ce3136f514d0274595cdd81542
Rafael T. C. Soares <rafaelcba@gmail.com>
2021-03-10 10:08:10 -0500

    removing duplicated test activator

--- kie-rules-archetype/src/main/resources/archetype-resources/src/test/java/com/redhat/demos/tests/KogitoScenarioJunitActivatorTest.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.demos.tests;
-/**
-* Use this class if you play to run scesim on VSCode with Kogito.
-*/
-// uncomment when executing from maven or vscode
-// @org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
-public class KogitoScenarioJunitActivatorTest {
-}
--- kie-rules-archetype/src/main/resources/archetype-resources/src/test/java/com/redhat/demos/tests/ScenarioJunitActivatorTest.java
+++ kie-rules-archetype/src/main/resources/archetype-resources/src/test/java/com/redhat/demos/tests/ScenarioJunitActivatorTest.java
@@ -3,6 +3,8 @@ package com.redhat.demos.tests;
 * Use this class if you play to run scesim on Business Central with Drools.
 * Remember to comment out the Drools scenariosimulation deps in pom.xml!
 */
+// uncomment when executing from maven or vscode
+// @org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
 @org.junit.runner.RunWith(org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator.class)
 public class ScenarioJunitActivatorTest {
 }
